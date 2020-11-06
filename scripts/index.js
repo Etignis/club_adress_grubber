4b48fcb51a7e42f4c9e8e5e667b93fe0def8d748
Etignis <etignis@gmail.com>
2020-11-06 14:32:29 +0300

    feat[vk api]: geat all data from API

--- scripts/index.js
+++ scripts/index.js
@@ -6,11 +6,16 @@ const fs = require('fs');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const path = require('path');
+const dotenv = require('dotenv');
 //const showdown  = require('showdown');
 //const request = require('request');
 //const fetch = require('node-fetch');
 const axios = require('axios');
 
+
+dotenv.config();
+const { VK_APP_KEY } = process.env;
+const vk_api = 'https://api.vk.com/method';
 const sStartPath = __dirname;
 //const cp = require('child_process');
 
@@ -18,7 +23,7 @@ const sStartPath = __dirname;
 
 
 function get_vk_links(){	
-	let filePath = path.join(sStartPath,'links.txt');
+	let filePath = path.join(sStartPath, '../data', 'links.txt');
 	let oFile = fs.readFileSync(filePath, {encoding : 'utf8'});
 	let aVkLinks = oFile.match(/https?:\/\/vk.com\/[a-z0-9_-]+/g);
 	return aVkLinks;
@@ -76,6 +81,7 @@ async function get_vk_group_data(sGroupURL){
 	
 }
 
+// "грабим" без API 
 async function get_vk_data(aLinks){
 	let aData = [];
 	for (let i=0; i<aLinks.length; i++) {
@@ -88,15 +94,95 @@ async function get_vk_data(aLinks){
 	return aData;
 }
 
+
+async function _vkAPI(sMethod, oParams){
+	let aParams=[
+		`access_token=${VK_APP_KEY}`,
+		`v=5.124`,
+	];
+	for (let key in oParams) {
+		aParams.push(`${key}=${oParams[key]}`);
+	}
+	let sApiUrl=`${vk_api}/${sMethod}?${aParams.join('&')}`;
+	
+	return new Promise((resolve, reject) => {
+	axios.get(sApiUrl)
+		.then(function (response) {
+			if(response && response.data && response.data.response) {				
+				resolve(response.data.response);		
+			}	else {
+				reject({error: 'Не удалось получить данные групп', data: response});
+			}
+		})
+		.catch(function (error) {
+			console.log(error);
+			reject(error);
+		})
+		
+	})
+		
+}
+
+async function  get_groups_data_by_api(aLinks){
+	let aNames = aLinks.map(el=>el.replace('https://vk.com/', ''));
+	let aGroupsData = await _vkAPI('groups.getById', 
+		{
+			group_ids: aNames.join(),
+			fields: [
+				'name',
+				'deactivated',
+				'country',
+				'city',
+				'place',
+				'addresses',
+				//'contacts',
+				'description'				
+			].join()
+		}
+	);
+	
+	if(aGroupsData){
+		return aGroupsData;
+	}
+}
+async function add_groups_addresses(aGroups){
+	for (let i=0; i<aGroups.length; i++) {
+		if(aGroups[i].addresses) {
+			let aGroupAddress= await _vkAPI('groups.getAddresses', 
+				{
+					group_id: aGroups[i].id,
+					address_ids: aGroups[i].addresses.main_address_id,
+					fields: [
+						'title',
+						'address',
+						'city',
+						'country',
+						'place'
+					].join()
+				}
+			);
+			if(aGroupAddress && aGroupAddress.items && aGroupAddress.items[0]) {
+				aGroups[i].address = aGroupAddress.items[0].address;
+				aGroups[i].addressTitle = aGroupAddress.items[0].title;
+			}
+		}
+		
+	}
+
+	return aGroups;
+}
+
 function write_output(aData){
-	let filePath = path.join(sStartPath,'vk_data.txt');
-	let sFileData =  aData.map(oClubData=> `${oClubData.url}: \r\n ${oClubData.data}`).join('\r\n\r\n------\r\n\r\n')
+	let filePath = path.join(sStartPath, '../data', 'vk_data.txt');
+	let sFileData = JSON.stringify(aData, null, '  ') ;//  aData.map(oClubData=> `${oClubData.url}: \r\n ${oClubData.data}`).join('\r\n\r\n------\r\n\r\n')
 	fs.writeFileSync(filePath, sFileData);
 }
 
 async function  main() {
   let aVkLinks = get_vk_links();
-	let aVkData = await get_vk_data(aVkLinks);
+	//let aVkData = await get_vk_data(aVkLinks);
+	let aVkData = await get_groups_data_by_api(aVkLinks);
+	aVkData = await add_groups_addresses(aVkData);
 	//console.dir (aVkData);
 	write_output(aVkData);
 }
